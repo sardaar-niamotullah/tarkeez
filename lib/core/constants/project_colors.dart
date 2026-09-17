cbfb722e91374e9587b40859bf089a4c285a35b6
sardaar-niamotullah <sardaar.niamotullah@gmail.com>
2026-09-17 13:33:12 +0600

    feat: introduce project colors as consts

---
+++ lib/core/constants/project_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  static const List<Color> colors = [
+    Color(0xFF4ADE80),
+    Color(0xFF22C55E),
+    Color(0xFF15803D),
+    Color(0xFF60A5FA),
+    Color(0xFF3B82F6),
+    Color(0xFF1D4ED8),
+    Color(0xFFC084FC),
+    Color(0xFFA855F7),
+    Color(0xFF7E22CE),
+    Color(0xFFFACC15),
+    Color(0xFFEAB308),
+    Color(0xFFA16207),
+    Color(0xFFFB923C),
+    Color(0xFFF97316),
+    Color(0xFFC2410C),
+    Color(0xFFF472B6),
+    Color(0xFFEC4899),
+    Color(0xFFBE185D),
+    Color(0xFFFF6B6B),
+    Color(0xFFEF4444),
+    Color(0xFFB91C1C),
+  ];
+}
