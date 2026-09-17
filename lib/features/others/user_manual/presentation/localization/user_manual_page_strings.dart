d72d5f43bfa6b734a9ce8e67184882ed208a764d
sardaar-niamotullah <sardaar.niamotullah@gmail.com>
2026-09-17 17:11:43 +0600

    refactor

--- lib/features/others/user_manual/presentation/localization/user_manual_page_strings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tarkeez/core/shared_files/models/app_string_model.dart';
-
-class UserManualPageStrings {
-  static const manual = AppStringModel(en: 'Manual', bn: 'ব্যবহার বিধি');
-  static const userManual = AppStringModel(
-    en: 'User Manual',
-    bn: 'ব্যবহার বিধি',
-  );
-}
--- lib/features/report/presentation/localization/report_strings.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tarkeez/core/shared_files/models/app_string_model.dart';
-
-class ReportStrings {
-  static const reports = AppStringModel(en: 'Reports', bn: 'রিপোর্ট');
-}
