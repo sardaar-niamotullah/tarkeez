bcde5fdd0333d9eb1f97e2053e0dac4639ab4037
sardaar-niamotullah <sardaar.niamotullah@gmail.com>
2026-09-19 20:45:53 +0600

    feat: wire di

--- lib/core/app/app_providers.dart
+++ lib/core/app/app_providers.dart
@@ -1,19 +1,20 @@
-import 'package:tarkeez/core/dependency_injection/di.dart';
-import 'package:tarkeez/core/shared_files/cubits/navigation_cubit.dart';
-import 'package:tarkeez/core/shared_files/cubits/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tarkeez/core/dependency_injection/di.dart';
 import 'package:tarkeez/features/projects/bloc/project_bloc.dart';
+import 'package:tarkeez/features/sessions/bloc/session_bloc.dart';
+import 'package:tarkeez/core/shared_files/cubits/theme_cubit.dart';
+import 'package:tarkeez/core/shared_files/cubits/navigation_cubit.dart';
 
 class AppProviders extends StatelessWidget {
-  final Widget child;
-  final ThemeState initialTheme;
-
   const AppProviders({
     super.key,
     required this.child,
     required this.initialTheme,
   });
+  
+  final Widget child;
+  final ThemeState initialTheme;
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +23,9 @@ class AppProviders extends StatelessWidget {
         BlocProvider(create: (_) => ThemeCubit(initialState: initialTheme)),
         BlocProvider(create: (_) => NavigationCubit()),
         BlocProvider(create: (_) => getIt<ProjectBloc>()),
+        BlocProvider(create: (_) => getIt<SessionBloc>()),
       ],
-      child: child 
+      child: child,
     );
   }
 }
--- lib/core/dependency_injection/di.dart
+++ lib/core/dependency_injection/di.dart
@@ -5,6 +5,7 @@ import 'package:tarkeez/core/database/app_database.dart';
 import 'package:tarkeez/core/services/sound_service.dart';
 import 'package:tarkeez/features/projects/bloc/project_bloc.dart';
 import 'package:tarkeez/features/projects/data/repositories/project_repository.dart';
+import 'package:tarkeez/features/sessions/bloc/session_bloc.dart';
 import 'package:tarkeez/features/sessions/data/repositories/session_repository.dart';
 
 final getIt = GetIt.instance;
@@ -32,7 +33,7 @@ Future<void> injectDependencies() async {
   getIt.registerLazySingleton<SessionRepository>(
     () => SessionRepositoryImpl(getIt<AppDatabase>()),
   );
-  // getIt.registerLazySingleton<ProjectBloc>(
-  //   () => ProjectBloc(getIt<ProjectRepository>()),
-  // );
+  getIt.registerLazySingleton<SessionBloc>(
+    () => SessionBloc(getIt<SessionRepository>()),
+  );
 }
